12e310d6b9b6675b637bc4cadd2dda3f49fb35d8
Pocky <shan_jingbo@outlook.com>
2024-07-18 10:53:15 +0800

    2nd

--- utils/cluster.py
+++ utils/cluster.py
@@ -1,6 +1,24 @@
+#聚类
 from io_metric import *
-from Tst import *
+# from Tst import *
 import numpy as np
+import numpy.matlib
+from numpy import linalg as la
+from CONST import *
 import pandas as pd
 import pandapower as pp
+
+
+def half_metric(A):
+    # 求矩阵-1/2次方
+    # v 为特征值    Q 为特征向量
+    v, Q = la.eig(A)
+    V = np.diag(v ** (-0.5))
+    T = Q * V * la.inv(Q)
+    return T
+
+
+W, D, net = io_metric()
+D_half = half_metric(D)
+L = np.matlib.identity(BUS_NUM) - np.dot(D_half, W, D_half)
 
--- utils/io_metric.py
+++ utils/io_metric.py
@@ -1,4 +1,4 @@
-#输入，输出W, D
+#输入，输出W, D, net
 import scipy.io as sio
 import numpy as np
 import pandapower as pp
@@ -13,17 +13,20 @@ def jsonread(name):
     data = json.loads(json_str)
     return data
 
-data = jsonread('GBreducednetwork.json')
-connect = jsonread('connect.json')
-connect = connect['connect']
+def io_metric():
+    data = jsonread('GBreducednetwork.json')
+    connect = jsonread('connect.json')
+    connect = connect['connect']
 
-num_bus = len(connect)
+    num_bus = len(connect)
 
-W = np.zeros([num_bus, num_bus])
-for i in range(0,num_bus):
-    for j in range(0,num_bus):
-        if (j+1) in connect[i]:
-            W[i][j] = 1
-
-net = pp.create_empty_network()
-
+    W = np.zeros([num_bus, num_bus])
+    D = np.diag(np.ones(num_bus))
+    for i in range(0,num_bus):
+        for j in range(0,num_bus):
+            if (j+1) in connect[i]:
+                W[i][j] = 1
+    for i in range(0,num_bus):
+        D[i][i] = sum(W[i])
+    net = pp.create_empty_network()
+    return net, W, D
